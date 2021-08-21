3322d0605058e1a7b34fc1b008334ec8bf6d053e
iCode_Rx <arababs2015@gmail.com>
2021-08-21 16:02:55 +0100

    ..

--- server.js
+++ server.js
@@ -1,39 +1,91 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const bodyparser = require("body-parser");
-const path = require('path');
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
 
-// require('dotenv').config({path: './server/database/config.env'})
 
-const connectDB = require('./server/database/connection');
+let db,
+	dbConnectionStr = process.env.DB_STRING,
+	dbName = 'todo'
 
-const app = express();
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+	.then(client => {
+		console.log(`Connected to ${dbName} Database`)
+		db = client.db(dbName)
+	})
 
-// dotenv.config( { path : 'config.env'} )
-dotenv.config( { path : './server/database/config.env'} )
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-const PORT = process.env.PORT || 8080
 
-// log requests
-app.use(morgan('tiny'));
+app.get('/',async (request, response)=>{
+	const todoItems = await db.collection('todos').find().toArray()
+	const itemsLeft = await db.collection('todos').countDocuments({completed: false})
+	response.render('index.ejs', { items: todoItems, left: itemsLeft })
+	// db.collection('todos').find().toArray()
+	// .then(data => {
+	//     db.collection('todos').countDocuments({completed: false})
+	//     .then(itemsLeft => {
+	//         response.render('index.ejs', { items: data, left: itemsLeft })
+	//     })
+	// })
+	// .catch(error => console.error(error))
+})
 
-// mongodb connection
-connectDB();
+app.post('/', (request, response) => {
+	db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+	.then(result => {
+		console.log('Todo Added')
+		response.redirect('/')
+	})
+	.catch(error => console.error(error))
+})
 
-// parse request to body-parser
-app.use(bodyparser.urlencoded({ extended : true}))
+app.put('/markComplete', (request, response) => {
+	db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+		$set: {
+		completed: true
+		}
+	},{
+		sort: {_id: -1},
+		upsert: false
+	})
+	.then(result => {
+		console.log('Marked Complete')
+		response.json('Marked Complete')
+	})
+	.catch(error => console.error(error))
+})
 
-// set view engine
-app.set("view engine", "ejs")
-//app.set("views", path.resolve(__dirname, "views/ejs"))
+app.put('/markUnComplete', (request, response) => {
+	db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+		$set: {
+		completed: false
+		}
+	},{
+		sort: {_id: -1},
+		upsert: false
+	})
+	.then(result => {
+		console.log('Marked Complete')
+		response.json('Marked Complete')
+	})
+	.catch(error => console.error(error))
+})
 
-// load assets
-app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
-app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
-app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+app.delete('/deleteItem', (request, response) => {
+	db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+	.then(result => {
+		console.log('Todo Deleted')
+		response.json('Todo Deleted')
+	})
+	.catch(error => console.error(error))
 
-// load routers
-app.use('/', require('./server/routes/router'))
+})
 
-app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
+app.listen(process.env.PORT || PORT, ()=>{
+	console.log(`Server running on port ${PORT}`)
+})
